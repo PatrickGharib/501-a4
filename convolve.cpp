15b72b5678ea3c61fb7251b9ac04bfd99d54adbb
PatrickGharib <patrick.a.gharib@gmail.com>
2019-04-07 19:45:38 -0600

    change char, short, long to uint8_t.... open the file file and get the bytes need to figure out what to do with the data part of the wave file still thinking of adding it to a vector and then sending it to the convolve method

--- convolve.cpp
+++ convolve.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -9,20 +11,21 @@ using namespace std;
 typedef struct WAVEHEADER{
     //TODO change to uint32_t and stuff for these cause its apparently really bad 
     
-    char RIFF[4];
-    unsigned int ChunkSize;
+    uint8_t RIFF[4];
+    uint32_t ChunkSize;
     char format[4];
-    char Subchunk1ID; 
-    unsigned long Subchunk1Size;
-    unsigned short AudioFormat;
-    unsigned short NumChannels;
-    unsigned long SampleRate; 
-    unsigned long ByteRate;
-    unsigned short BlockAlign;
-    unsigned long BitsPerSample;
-    char Subchunk2ID[4];
-    unsigned long Subchunk2Size;
+    char Subchunk1ID;
+    uint32_t Subchunk1Size;
+    uint16_t AudioFormat;
+    uint16_t NumChannels;
+    uint16_t SampleRate; 
+    uint16_t ByteRate;
+    uint16_t BlockAlign;
+    uint16_t BitsPerSample;
+    uint8_t Subchunk2ID[4];
+    uint16_t Subchunk2Size;
 }wavHdr;
+int getWaveFileSize(FILE* fileSize);
 int main(int argc, char** argv){
     //check user input for right input
     if(argc != 4){
@@ -32,10 +35,25 @@ int main(int argc, char** argv){
 
     wavhdr wavHead;
     int sizeOfheader = sizeof(wavHeader), lengthOfFile = 0;
+    
     const char* inputFile = argv[1];
     //TODO need to move this to save some mem so that is not just floating
-    //const char* iRFile = argv[2];
-    FILE* wavFile = fopen
+    //const char* iRFile = argv[2];im
+    FILE* wavFile = fopen(inputFile, "r");
+    if (wavFile = nullptr){
+        fprintf(stderr, "Unable to open inputFile: %s\n", filePath);
+        return 1;
+    }
+
+    size_t bytesRead fread(&fread(&wavHeader,1, sizeOfheader, waveFile));
+    if (bytesRead > 0){
+        static const uint64_t BUFFERSIZE = 4096;
+        int8_t buffer = new int8_t[BUFFERSIZE];
+        vector wavData
+        while((bytesRead = fread(buffer, sizeof buffer[0], BUFFERSIZE/ (sizeof buffer[0]), wavFile)) > 0){
+            
+        }
+    }
 
     string inputFile = argv[1];
     string IRfile = argv[2];
