f9b40d64c365d3e4dd7fdf59aa189bd97dfde881
PatrickGharib <patrick.a.gharib@gmail.com>
2019-04-09 17:13:06 -0600

    currently reads in the two input wave files and stores the data into two buffer arrays , working on convolution aspect of program now

--- convolve.cpp
+++ convolve.cpp
@@ -4,7 +4,8 @@
 #include <string>
 #include <fstream>
 #include <vector> 
-
+#include <cstdint>
+#include <algorithm>
 using namespace std;
 
 //struct to organize wave info from input 
@@ -13,19 +14,25 @@ typedef struct WAVEHEADER{
     
     uint8_t RIFF[4];
     uint32_t ChunkSize;
-    char format[4];
-    char Subchunk1ID;
+    uint8_t format[4];
+    uint8_t Subchunk1ID;
     uint32_t Subchunk1Size;
     uint16_t AudioFormat;
     uint16_t NumChannels;
-    uint16_t SampleRate; 
-    uint16_t ByteRate;
+    uint32_t SampleRate; 
+    uint32_t ByteRate;
     uint16_t BlockAlign;
     uint16_t BitsPerSample;
     uint8_t Subchunk2ID[4];
-    uint16_t Subchunk2Size;
-}wavHdr;
-int getWaveFileSize(FILE* fileSize);
+    uint32_t Subchunk2Size;
+} wavHdr;
+
+//Prototyping
+void convolve(int8_t x[], int N, int8_t h[], int M, int8_t y[], int P);
+
+int getWaveFileSize(FILE *inFile);
+int8_t* getWavData(FILE *wavFile);
+
 int main(int argc, char** argv){
     //check user input for right input
     if(argc != 4){
@@ -33,37 +40,149 @@ int main(int argc, char** argv){
         exit(0);
     }
 
-    wavhdr wavHead;
-    int sizeOfheader = sizeof(wavHeader), lengthOfFile = 0;
+    wavHdr wavHeader1;
+    wavHdr wavHeader2;
+
+
+    int sizeOfHeader1 = sizeof(wavHeader1), lengthOfFile = 0;
+    int sizeOfHeader2 = sizeof(wavHeader2);
     
     const char* inputFile = argv[1];
+
     //TODO need to move this to save some mem so that is not just floating
     //const char* iRFile = argv[2];im
     FILE* wavFile = fopen(inputFile, "r");
-    if (wavFile = nullptr){
-        fprintf(stderr, "Unable to open inputFile: %s\n", filePath);
+
+    if (wavFile == NULL){
+        fprintf(stderr, "Unable to open inputFile: %s\n", inputFile);
         return 1;
     }
 
-    size_t bytesRead fread(&fread(&wavHeader,1, sizeOfheader, waveFile));
-    if (bytesRead > 0){
-        static const uint64_t BUFFERSIZE = 4096;
-        int8_t buffer = new int8_t[BUFFERSIZE];
-        vector wavData
-        while((bytesRead = fread(buffer, sizeof buffer[0], BUFFERSIZE/ (sizeof buffer[0]), wavFile)) > 0){
-            
+    //read in the header
+    size_t bytesRead = fread(&wavHeader1, 1, sizeOfHeader1, wavFile);
+    uint16_t bytesPerSample1 = wavHeader1.BitsPerSample/8;
+    int64_t numberOfSamples1 = wavHeader1.ChunkSize/bytesPerSample1;
+    //if (bytesRead > 0){
+        static const uint64_t BUFFERSIZE = wavHeader1.Subchunk2Size;
+        int8_t* buffer1 = new int8_t[BUFFERSIZE];
+        //TODO add vector record the data part of wave
+        while((bytesRead = fread(buffer1, sizeof buffer1[0], BUFFERSIZE / (sizeof buffer1[0]), wavFile)) > 0){   
+        //cout << "Re " << buffer << " bytes." << endl;
         }
+         //cout << "penis";
+
+        delete [] buffer1;
+        buffer1 = nullptr;
+       //fileLength = getFileSize(wavFile);
+    //} 
+    
+
+
+
+
+
+
+
+
+
+   const char* iRFile = argv[2];
+
+   wavFile = fopen(iRFile, "r");
+
+    if (wavFile == NULL){
+        fprintf(stderr, "Unable to open IRFile: %s\n", iRFile);
+        return 1;
     }
 
-    string inputFile = argv[1];
-    string IRfile = argv[2];
-    string outputFile = argv[3];
-    cout << inputFile << " " << IRfile << " " << outputFile;
+    //read in the header
+    bytesRead = fread(&wavHeader2, 1, sizeOfHeader2, wavFile);
+    uint16_t bytesPerSample2 = wavHeader2.BitsPerSample/8;
+    int64_t numberOfSamples2 = wavHeader2.ChunkSize/bytesPerSample2;
+   // if (bytesRead > 0){
+      
+        //TODO extract this into a method so that 
+       
+        // cout << bytesPerSample2 << ", " << numberOfSamples2;
+        static const uint64_t BUFFERSIZE2 = wavHeader2.Subchunk2Size;
+        int8_t* buffer2 = new int8_t[BUFFERSIZE2];
+    
+        //TODO add vector record the data part of wave
+        while((bytesRead = fread(buffer2, sizeof buffer2[0], BUFFERSIZE2 / (sizeof buffer2[0]), wavFile)) > 0){   
+            //cout << "Re " << buffer << " bytes." << endl;
+        }
+       
+       // delete [] buffer1;
+       // buffer1 = nullptr;
+        //fileLength = getFileSize(wavFile);
+      //}
 
 
+    fclose(wavFile);
+    
+
+    int64_t outputNumberOfSamples = numberOfSamples1 + numberOfSamples2 - 1;
+    int8_t* outputArray = new int8_t[outputNumberOfSamples];
+    cout << numberOfSamples1 << "," << numberOfSamples2 << "," << outputNumberOfSamples;
+    convolve(buffer1, numberOfSamples1, buffer2, numberOfSamples2, outputArray, outputNumberOfSamples);
+
+    return 0;
 
 }
-int getWaveFileSize(File* fileSize);
-void getWaveData(){
 
+int getWaveFileSize(FILE *inFile){
+    int fileSize = 0; 
+    fseek(inFile, 0, SEEK_END);
+    fileSize = ftell(inFile); fseek(inFile,0, SEEK_SET); 
+    return fileSize;
+}
+//void getWaveData(){
+
+/*****************************************************************************
+*
+*    Function:     convolve
+*    author: Leonard Manzara
+*
+*    Description:  Convolves two signals, producing an output signal.
+*                  The convolution is done in the time domain using the
+*                  "Input Side Algorithm" (see Smith, p. 112-115).
+*
+*    Parameters:   x[] is the signal to be convolved
+*                  N is the number of samples in the vector x[]
+*                  h[] is the impulse response, which is convolved with x[]
+*                  M is the number of samples in the vector h[]
+*                  y[] is the output signal, the result of the convolution
+*                  P is the number of samples in the vector y[].  P must
+*                       equal N + M - 1
+*
+*****************************************************************************/
+
+void convolve(float x[], int N, float h[], int M, float y[], int P)
+{
+  int n, m;
+    
+  /*  Make sure the output buffer is the right size: P = N + M - 1  */
+  if (P != (N + M - 1)) {
+    printf("Output signal vector is the wrong size\n");
+    printf("It is %-d, but should be %-d\n", P, (N + M - 1));
+    printf("Aborting convolution\n");
+    
+    return;
+  }
+
+  /*  Clear the output buffer y[] to all zero values  */  
+  for (n = 0; n < P; n++){
+    y[n] = 0.0;
+   // cout << y[n];
+    }
+ cout<<"Penis";
+  /*  Do the convolution  */
+  /*  Outer loop:  process each input value x[n] in turn  */
+  for (n = 0; n < N; n++) {
+    
+    /*  Inner loop:  process x[n] with each sample of h[]  */
+    for (m = 0; m < M; m++)
+      {y[n+m] += x[n] * h[m];
+       cout<<y[n+m];
+     }
+  }
 }
